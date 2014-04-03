95b271a5b4ef10217832ad6741c09588477ff92a
Sam Thompson <git@samt.us>
2014-04-03 14:21:02 -0700

    Basic gitolite() function

--- index.js
+++ index.js
@@ -4,3 +4,21 @@
  * (c) 2014 Sam Thompson <contact@samt.us>
  * The MIT License 
  */
+
+var git = require('gift'),
+  AdminRepo = require('./lib/adminrepo');
+
+function gitolite(path, cb) {
+  var repo = git(path),
+    adminRepo = null,
+    err = 'E_REPO_NOT_FOUND';
+
+  if (!repo) {
+    adminRepo = new AdminRepo(repo);
+    err = false;
+  }
+
+  cb(err, adminRepo);
+}
+
+module.exports = gitolite;
